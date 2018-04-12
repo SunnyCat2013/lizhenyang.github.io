21940b6f13b09d4624b1c07e4d2bebe10d1e7b79
SunnyCat2013 <lizhenyang_2008@163.com>
2018-04-12 16:11:06 +0800

    read images and labels

--- posts/lmdb/main.py
+++ posts/lmdb/main.py
@@ -8,6 +8,16 @@ import glob
 img_path = './image'
 
 img_list = glob.glob('./image/*.jpg')
+label_list = []
+
+def readText(path):
+    with open(path) as f:
+        return f.read().strip()
+
 for i in img_list:
     print i
+    label = readText(i.replace('.jpg', '.txt'))
+    label_list.append(label)
+    print label
 
+print len(img_list)
