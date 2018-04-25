5bd6b6b1b7b2833650ff79c8776def99261e45f9
lizhenyang <lizhenyang_2008@163.com>
2018-04-25 14:31:43 +0800

    add examples for yield

--- posts/python/generator.md
+++ posts/python/generator.md
@@ -1,1 +1,15 @@
 # 学习 generator
+
+```python
+ ❯ python generator.py                                                                                                                                                                           [11:13:03]
+ 第 1 次打印
+ 第 2 次打印
+ 第 3 次打印
+ 第 4 次打印
+ 第 5 次打印
+ ig.send(100)
+ 第 7 次打印
+ ```
+
+ # reference
+ [The Python yield keyword explained](https://pythontips.com/2013/09/29/the-python-yield-keyword-explained/)
--- posts/python/generator.py
+++ posts/python/generator.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
 # learning generator
+
+def yield_i(i = 0, cap = 100):
+    while i < cap:
+        yield i
+        i = i + 1
+
+
+ig = yield_i()
+
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('ig.send(100)')
+print('第 {} 次打印'.format(ig.send(100) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
+print('第 {} 次打印'.format(next(ig) + 1))
