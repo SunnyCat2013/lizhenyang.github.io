c059dad5bae995cd60a0fcdd9a52d32c920fde95
lizhenyang <lizhenyang@jd.com>
2018-10-07 13:49:29 +0800

    update polymorphism cpp

--- posts/cplusplus/polymorphism/polymorphism/main.cpp
+++ posts/cplusplus/polymorphism/polymorphism/main.cpp
@@ -18,16 +18,17 @@ public:
         width = a;
         height = b;
     }
-    virtual int area()
-    {
-        cout << "Parent class area :" <<endl;
-        return 0;
-    }
+    virtual int area() = 0;
+//    virtual int area()
+//    {
+//        cout << "Parent class area :" <<endl;
+//        return 0;
+//    }
 };
 class Rectangle: public Shape{
 public:
     Rectangle( int a=0, int b=0):Shape(a, b) { }
-    int area ()
+    virtual int area ()
     {
         cout << "Rectangle class area :" <<endl;
         return (width * height);
@@ -36,10 +37,19 @@ public:
 class Triangle: public Shape{
 public:
     Triangle( int a=0, int b=0):Shape(a, b) { }
-    int area ()
+    virtual int area ()
     {
         cout << "Triangle class area :" <<endl;
         return (width * height / 2);
+    }
+};
+class Square: public Rectangle{
+public:
+    // shape 的儿子类的 area 是虚函数吗？如果是，那么在孙子类 Square 中应该可以覆盖。
+    Square(int a, int b): Rectangle(a, b) {}
+    int area (int a) {
+        cout << "Square class area" << endl;
+        return width;
     }
 };
 // 程序的主函数
@@ -47,6 +57,7 @@ int main( )
 {
     Shape *shape;
     Rectangle rec(10,7);
+    Square squ(10, 7);
     Triangle  tri(10,5);
     
     // 存储矩形的地址
@@ -58,6 +69,13 @@ int main( )
     shape = &tri;
     // 调用三角形的求面积函数 area
     shape->area();
+    
+    shape = &squ;
+    shape->area(); // 预测：Square class area
+    
+    Rectangle * rectangle;
+    rectangle = &squ;
+    rectangle->area();
     
     return 0;
 }
